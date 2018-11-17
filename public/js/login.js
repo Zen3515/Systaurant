5b10c25c36a9ecf426ea81ec9457b13d7ee435a6
Natchapon Saejueng <gui_desan@outlook.com>
2018-11-17 17:41:36 +0700

    Send api request using axios

--- public/js/login.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    connect("/api/login", {
-        type: "employee",
-        id: $("#id").val(),
-        password: $("#password").val()
-    }, res => {
-        console.log(res.message);
-    })
-}
